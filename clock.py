35e6835277a5e773e05b4fa720f3202bf28bda6c
Avik Sarkar <76903360+Avik2903@users.noreply.github.com>
2022-10-04 23:57:35 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -7,6 +7,7 @@ def time():
 	string = strftime('%H:%M:%S %p')
 	lbl.config(text = string)
 	lbl.after(1000, time)
+	
 lbl = Label(root, font = ('calibri', 40, 'bold'),
 			background = 'purple',
 			foreground = 'white')
